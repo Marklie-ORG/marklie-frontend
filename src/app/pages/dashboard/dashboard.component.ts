576c8793c01a372ab653d841fa72cdafde01671d
Andrii Derevianko <59490330+DereviankoAndrew@users.noreply.github.com>
2025-04-24 12:01:08 +0200

    fb login service

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -5,6 +5,7 @@ import { OnboardingService } from '../../services/api/onboarding.service';
 import { Client, ClientService } from '../../services/api/client.service';
 import { MatDialog } from '@angular/material/dialog';
 import { AddClientComponent } from 'src/app/components/add-client/add-client.component';
+import { FacebookLoginService } from 'src/app/services/facebook-login.service';
 
 interface Activity {
   id: string;
@@ -57,7 +58,8 @@ export class DashboardComponent implements OnInit {
     private router: Router,
     private onboardingService: OnboardingService,
     private clientService: ClientService,
-    private dialog: MatDialog
+    private dialog: MatDialog,
+    private facebookLoginService: FacebookLoginService
   ) {}
 
   async ngOnInit() {
@@ -96,13 +98,8 @@ export class DashboardComponent implements OnInit {
     this.clients.push(newClient);
   }
 
-  fbLogin() {
-    const appId = '1187569946099353';
-    const redirectUri = 'https://derevian.co/saas/fb-login-callback';
-    const scope = 'ads_management,ads_read,business_management,catalog_management,commerce_account_manage_orders,commerce_account_read_orders,commerce_account_read_reports,commerce_account_read_settings,instagram_basic,instagram_content_publish,instagram_manage_comments,instagram_manage_events,instagram_manage_insights,instagram_manage_messages,instagram_shopping_tag_products,leads_retrieval,manage_fundraisers,page_events,pages_manage_ads,pages_show_list,pages_manage_cta,pages_manage_engagement,pages_manage_instant_articles,pages_manage_metadata,pages_manage_posts,pages_messaging,pages_read_engagement,pages_read_user_content,publish_video,read_insights,read_page_mailboxes,whatsapp_business_manage_events,whatsapp_business_management,whatsapp_business_messaging';
-
-    const authUrl = `https://www.facebook.com/v22.0/dialog/oauth?client_id=${appId}&redirect_uri=${redirectUri}&scope=${scope}`;
-    window.location.href = authUrl;
+  connectFacebook() {
+    this.facebookLoginService.connectFacebook();
   }
 
   viewReport(clientId: string) {
--- src/app/pages/onboarding/onboarding.component.ts
+++ src/app/pages/onboarding/onboarding.component.ts
@@ -12,6 +12,7 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
 import { UserService } from 'src/app/services/api/user.service';
 import { OrganizationService } from 'src/app/services/api/organization.service';
 import { environment } from '@env/environment';
+import { FacebookLoginService } from 'src/app/services/facebook-login.service';
 
 interface Platform {
   name: string;
@@ -89,7 +90,8 @@ export class OnboardingComponent implements OnInit {
     private onboardingService: OnboardingService,
     private router: Router,
     private userService: UserService,
-    private organizationService: OrganizationService
+    private organizationService: OrganizationService,
+    private facebookLoginService: FacebookLoginService
   ) {}
 
   async ngOnInit() {
@@ -202,18 +204,7 @@ export class OnboardingComponent implements OnInit {
   }
 
   connectFacebook() {
-    const appId = '1187569946099353';
-    const redirectUri = environment.facebookLoginCallbackUrl;
-    // const redirectUri = 'http://192.168.89.185:4200/fb-login-callback';
-    // const redirectUri = 'http://localhost:4200/fb-login-callback';
-    const scope = 'ads_management,ads_read,business_management,catalog_management,commerce_account_manage_orders,commerce_account_read_orders,commerce_account_read_reports,commerce_account_read_settings,instagram_basic,instagram_content_publish,instagram_manage_comments,instagram_manage_events,instagram_manage_insights,instagram_manage_messages,instagram_shopping_tag_products,leads_retrieval,manage_fundraisers,page_events,pages_manage_ads,pages_show_list,pages_manage_cta,pages_manage_engagement,pages_manage_instant_articles,pages_manage_metadata,pages_manage_posts,pages_messaging,pages_read_engagement,pages_read_user_content,publish_video,read_insights,read_page_mailboxes,whatsapp_business_manage_events,whatsapp_business_management,whatsapp_business_messaging';
-
-    const authUrl = `https://www.facebook.com/v22.0/dialog/oauth?client_id=${appId}&redirect_uri=${redirectUri}&scope=${scope}`;
-    
-    // Open Facebook login dialog in a new tab
-    // window.open(authUrl, '_blank');
-    window.location.href = authUrl;
-
+    this.facebookLoginService.connectFacebook();
   }
 
   answerHowDidYouHear() {

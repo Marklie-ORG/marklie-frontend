db1cd3e8437709a4ffd06c1be3cb08b3978a0fb6
romapchel <notpchelintsevroma@gmail.com>
2025-07-16 20:12:38 +0300

    correct prod environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,17 +1,17 @@
 export const environment = {
   production: false,
 
-  // facebookLoginCallbackUrl: 'https://marklie.com/fb-login-callback',
-  // reportsApiUrl: 'https://reports-service-10669311980.europe-west4.run.app/api',
-  // apiUrl: 'https://agencies-api.marklie.com/api',
-  // authApiUrl: 'https://agencies-api.marklie.com/api',
-  // slackLoginCallbackUrl: 'https://marklie.com/slack-login-callback',
+  facebookLoginCallbackUrl: 'https://marklie.com/fb-login-callback',
+  reportsApiUrl: 'https://reports-service-10669311980.europe-west4.run.app/api',
+  apiUrl: 'https://agencies-api.marklie.com/api',
+  authApiUrl: 'https://agencies-api.marklie.com/api',
+  slackLoginCallbackUrl: 'https://marklie.com/slack-login-callback',
 
-  reportsApiUrl: 'http://localhost:3030/api',
-  apiUrl: 'http://localhost:3001/api',
-  authApiUrl: 'http://localhost:3001/api',
-  facebookLoginCallbackUrl: 'http://localhost:4200/fb-login-callback',
-  slackLoginCallbackUrl: 'https://redirectmeto.com/http://localhost:4200/slack-login-callback',
+  // reportsApiUrl: 'http://localhost:3030/api',
+  // apiUrl: 'http://localhost:3001/api',
+  // authApiUrl: 'http://localhost:3001/api',
+  // facebookLoginCallbackUrl: 'http://localhost:4200/fb-login-callback',
+  // slackLoginCallbackUrl: 'https://redirectmeto.com/http://localhost:4200/slack-login-callback',
 
   version: '1.0.0',
   debug: true

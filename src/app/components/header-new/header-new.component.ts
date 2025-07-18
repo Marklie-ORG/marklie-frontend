cd4ee9221be05b4d62039a47f549d2f724325bc1
dima <d.lebedev@svit.io>
2025-07-18 22:47:03 +0300

    feat: create new header setup

---
+++ src/app/components/header-new/header-new.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header-new',
+  templateUrl: './header-new.component.html',
+  styleUrls: ['./header-new.component.scss'],
+})
+export class HeaderNewComponent {
+}

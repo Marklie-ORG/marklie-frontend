56d8644102a3498b9d5cf9801c1a641064d30cbd
Andrii Derevianko <59490330+DereviankoAndrew@users.noreply.github.com>
2025-07-19 13:06:17 +0200

    change fb dev account for db login

--- src/app/services/api/facebook-login.service.ts
+++ src/app/services/api/facebook-login.service.ts
@@ -8,7 +8,7 @@ import {environment} from '@env/environment.js';
 export class FacebookLoginService {
 
     connectFacebook() {
-        const appId = '1187569946099353';
+        const appId = '1042744144657355';
         const redirectUri = environment.facebookLoginCallbackUrl;
         const scope = 'ads_management,ads_read,business_management,instagram_basic,instagram_manage_insights,pages_show_list,pages_read_engagement,read_insights';
 

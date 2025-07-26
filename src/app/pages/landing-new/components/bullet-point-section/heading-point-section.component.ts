c7e19f57f85a3f7d68baa782f2ba2b9bb652dbb0
dima <d.lebedev@svit.io>
2025-07-26 11:20:03 +0300

    feat: add heading point section

--- src/app/pages/landing-new/components/bullet-point-section/heading-point-section.component.ts
+++ src/app/pages/landing-new/components/bullet-point-section/heading-point-section.component.ts
@@ -1,15 +1,14 @@
-import { Component, input } from "@angular/core";
-import { CommonModule } from "@angular/common";
+import { Component, input, computed } from "@angular/core";
 
 @Component({
   selector: "heading-point-section",
   standalone: true,
-  imports: [CommonModule],
   templateUrl: "./heading-point-section.component.html",
   styleUrls: ["./heading-point-section.component.scss"]
 })
 export class HeadingPointSectionComponent {
-  title = input<string>("");
+  titleLarge = input<string>("");
+  titleSmall = input<string>("");
   subTitle = input<string>("");
   description = input<string>("");
 }
--- src/app/pages/landing-new/landing-new.component.ts
+++ src/app/pages/landing-new/landing-new.component.ts
@@ -1,5 +1,5 @@
 import { Component } from "@angular/core";
-import { HeadingPointSectionComponent } from "./components/heading-point-section/heading-point-section.component";
+import { HeadingPointSectionComponent } from "./components/bullet-point-section/heading-point-section.component";
 import { CommonModule } from "@angular/common";
 import { LandingHeroSectionComponent } from "./components/landing-hero-section/landing-hero-section..component";
 import { LandingCommunicationSectionComponent } from "./components/landing-communication-section/landing-communication-section.component";
